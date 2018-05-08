ae9d642dd5b3a39370cbe1effe6e12350e72ae57
Manoj Gautam <nix1947@users.noreply.github.com>
2018-05-08 16:37:19 +0545

    update example.py with example

--- src/1/calculating_with_dictionaries/example.py
+++ src/1/calculating_with_dictionaries/example.py
@@ -11,6 +11,12 @@ prices = {
 }
 
 # Find min and max price
+# Comparision is done based on the first value
+# Combine three values and return the list of tupele.
+# Example: >>> zip([1,2,3],[4,5,6],[7,8,9])
+#  [(1, 4, 7), (2, 5, 8), (3, 6, 9)]
+
+
 min_price = min(zip(prices.values(), prices.keys()))
 max_price = max(zip(prices.values(), prices.keys()))
 
@@ -18,6 +24,8 @@ print('min price:', min_price)
 print('max price:', max_price)
 
 print('sorted prices:')
+# Sort based on the first tuple value
+# Nature of sorted function is sort the value based on first index value in case of iterable of iterable.
 prices_sorted = sorted(zip(prices.values(), prices.keys()))
 for price, name in prices_sorted:
     print('    ', name, price)
